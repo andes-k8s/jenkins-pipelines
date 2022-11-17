18246027ff6ca4176e4a50aaf74a9542bd9a98e8
Orlando Brea <orlando.brea@gmail.com>
2022-11-17 15:11:21 -0300

    feat: runAnsible test pipeline

--- libraries/vars/runAnsible.groovy
+++ libraries/vars/runAnsible.groovy
@@ -27,8 +27,8 @@ def call(body) {
   def pubKeyFileName = "id_rsa"
   convertValueToFile(userPublicKey, pubKeyFileName)
   sh "ls -lah"
-  sh "docker run --rm -v ${playbookFileName}:/ansible/${playbookFileName} -v ${hostsFileName}:/ansible/${hostsFileName} -v ${pubKeyFileName}:/root/.ssh/id_rsa --workdir=/ansible ${ansibleImage} ls -lah"
-  sh "docker run --rm -v ${playbookFileName}:/ansible/${playbookFileName} -v ${hostsFileName}:/ansible/${hostsFileName} -v ${pubKeyFileName}:/root/.ssh/id_rsa --workdir=/ansible ${ansibleImage} ansible-playbook -i ${hostsFileName} ${playbookFileName}"
+  sh "docker run --rm -v ./${playbookFileName}:/ansible/${playbookFileName} -v ./${hostsFileName}:/ansible/${hostsFileName} -v ${pubKeyFileName}:/root/.ssh/id_rsa --workdir=/ansible ${ansibleImage} ls -lah"
+  sh "docker run --rm -v ./${playbookFileName}:/ansible/${playbookFileName} -v ./${hostsFileName}:/ansible/${hostsFileName} -v ${pubKeyFileName}:/root/.ssh/id_rsa --workdir=/ansible ${ansibleImage} ansible-playbook -i ${hostsFileName} ${playbookFileName}"
 
 
 }
