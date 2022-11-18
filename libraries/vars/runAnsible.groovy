ef715ad8c80b30c22c50669cd2fa90a7dfc60650
Orlando Brea <orlando.brea@gmail.com>
2022-11-18 08:43:54 -0300

    fix: change ssh key permissions

--- libraries/vars/runAnsible.groovy
+++ libraries/vars/runAnsible.groovy
@@ -21,11 +21,11 @@ def call(body) {
   def hostsFileName = createFileFrom(config.hosts, config.hostFile, config.hostsFromParams, "inventory.ini", params)
   def userPublicKey = getFromValueOrParams(config.userPublicKey, config.userPublicKeyFromParams)
   def pubKeyFileName = "id_rsa"
-  sh "chmod 777 id_rsa"
-  sh "rm -f id_rsa"
   convertValueToFile(userPublicKey, pubKeyFileName)
+  sh "chmod 400 id_rsa"
   sh "ls -lah"
   sh "docker run --rm -v \$(pwd)/${playbookFileName}:/ansible/${playbookFileName} -v \$(pwd)/${hostsFileName}:/ansible/${hostsFileName} -v ${pubKeyFileName}:/root/.ssh/id_rsa:ro --workdir=/ansible ${ansibleImage} -i ${hostsFileName} ${playbookFileName}"
+  sh "rm -f id_rsa"
 
 
 }
