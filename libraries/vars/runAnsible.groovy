8d242fe0146b94786f4c568f74b2f2976b8ebf40
Orlando Brea <orlando.brea@gmail.com>
2022-11-18 12:23:01 -0300

    fix: clean-k8s-logs

--- libraries/vars/runAnsible.groovy
+++ libraries/vars/runAnsible.groovy
@@ -18,13 +18,18 @@ def call(body) {
 
   sh "ls -lah"
   sh "ls playbook.yml"
-  sh "rm -f id_rsa"
+  sh "rm -Rf id_rsa"
+  sh "rm -Rf playbook.yml"
+  sh "rm -Rf inventory.ini"
   def playbookFileName = createFileFrom(config.playbook, config.playbookFile, config.playbookFromParams, "playbook.yml", params)
   def hostsFileName = createFileFrom(config.hosts, config.hostFile, config.hostsFromParams, "inventory.ini", params)
   def userPrivateKey = getFromValueOrParams(config.userPrivateKey, config.userPrivateKeyFromParams)
   def privKeyFileName = "id_rsa"
   convertValueToFile(userPrivateKey, privKeyFileName)
   sh "ls -lah"
+  sh "rm -Rf id_rsa"
+  sh "rm -Rf playbook.yml"
+  sh "rm -Rf inventory.ini"
   sh "docker run --rm -v \$(pwd)/${playbookFileName}:/app/${playbookFileName} -v \$(pwd)/${hostsFileName}:/app/${hostsFileName} -v \$(pwd)/${privKeyFileName}:/root/.ssh/id_rsa ${ansibleImage} -i ${hostsFileName} /app/${playbookFileName}"
 
 
