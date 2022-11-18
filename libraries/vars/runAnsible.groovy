edddc2906d8c02bd1a9d00f3ac500490398f0076
Orlando Brea <orlando.brea@gmail.com>
2022-11-18 14:18:43 -0300

    fix: clean-k8s-logs

--- libraries/vars/runAnsible.groovy
+++ libraries/vars/runAnsible.groovy
@@ -28,11 +28,6 @@ def call(body) {
   // def privKeyFileName = "id_rsa"
   // convertValueToFile(userPrivateKey, privKeyFileName)
   sh "ls -lah"
-  sh "ls -lah ../"
-  sh "ls -lah ../clean-k8s-logs-new"
-  sh "ls -lah ../clean-k8s-logs-new@libs"
-  sh "ls -lah ../clean-k8s-logs-new@script"
-  sh "ls -lah ../clean-k8s-logs-new@tmp"
   // sh "docker run --rm -v \$(pwd)/${playbookFileName}:/app/${playbookFileName} -v \$(pwd)/${hostsFileName}:/app/${hostsFileName} -v \$(pwd)/${privKeyFileName}:/root/.ssh/id_rsa ${ansibleImage} -i ${hostsFileName} /app/${playbookFileName}"
 
   sh "rm -Rf id_rsa"
