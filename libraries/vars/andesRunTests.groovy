9eca2abe672356aeb9450d6932ace8f3218431e9
Orlando Brea <orlando.brea@gmail.com>
2022-11-29 11:25:29 -0300

    feat: andesRunTests try

--- libraries/vars/andesRunTests.groovy
+++ libraries/vars/andesRunTests.groovy
@@ -5,20 +5,43 @@ def call(body) {
   body.delegate = config
   body()
 
-  // params: repoUrl, cypressImage, apiTag, appTag 
-  // Steps 
-  // clone 
-  // npm ci 
-  // compose up 
-  // run test 
-  def testsStages = [:]
-  for(def i=1; i<4; i++) {
-    def currentValue = i
-    testsStages[i] = {
-      echo "running in parallel ${currentValue}"
-      sh "docker ps -a"
-    }
-  }
-  parallel testsStages
+  def repoUrl = null 
+  def branch = "master"
+  def cypressImage = "cypress/browsers:node14.19.0-chrome100-ff99-edge" 
+  def dockerApiTag = "master"
+  def dockerAppTag = "master"
+  if (config != null) {
+    repoUrl = config.repoUrl ? config.repoUrl : ""
+    branch = config.branchName ? config.branchName : (config.branchFromParam ? params[config.branchFromParam] : branch) 
+    cypressImage = config.cypressImage ? config.cypressImage : cypressImage
+    dockerApiTag = config.dockerApiTag ? config.dockerApiTag : dockerApiTag
+    dockerAppTag = config.dockerAppTag ? config.dockerAppTag : dockerAppTag
+    // params: repoUrl, cypressImage, dockerApiTag, dockerAppTag 
+    // Steps 
+    // clone 
+    // npm ci 
+    // compose up 
+    // run test 
+    def checkoutResponse = checkout([
+        $class: 'GitSCM',
+        branches: [[name:  branch ]],
+        userRemoteConfigs: [[ url: repoUrl]]
+    ])
+    def testsStages = [:]
+    for(def i=1; i<5; i++) {
+      def currentValue = i
+      testsStages[i] = {
+        node {
+          sh 'API=$dockerApiTag APP=$dockerAppTag docker-compose -p andes-${currentValue}-${BUILD_NUMBER} -f docker/docker-compose.yml up -d'
+          sh 'sleep 10'
+          // run tests 
 
+          echo "running in parallel ${currentValue}"
+          sh "docker ps -a"
+          sh "docker-compose -f docker/docker-compose.yml down"
+        }
+      }
+    }
+    parallel testsStages
+  }
 }
